5fe6e2e4cf2ee34348901d59cb803ee0367e0735
Nayan Prakash <107613181+nayanjee@users.noreply.github.com>
2023-12-08 11:14:41 +0530

    config modification

---
+++ app/config/db.config.js
@@ -0,0 +1,17 @@
+/**
+    * @description      : 
+    * @author           : nayan.prakash
+    * @group            : 
+    * @created          : 01/05/2023 - 10:50:59
+    * 
+    * MODIFICATION LOG
+    * - Version         : 1.0.0
+    * - Date            : 01/05/2023
+    * - Author          : nayan.prakash
+    * - Modification    : 
+**/
+module.exports = {
+  HOST: "localhost",
+  PORT: 27017,
+  DB: "dataplex"
+};
